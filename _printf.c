c88e265027e82776d95e066add6c5b1803397603
Abouzaid1 <am5715073@gmail.com>
2023-11-13 16:15:31 +0200

    some changes

--- _printf.c
+++ _printf.c
@@ -1,42 +1,46 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
+/**
+ * _printf - print to stdout formatted text
+ *
+ * @format: formate specifier
+ * Return: number of bytes printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i, s_count;
+	int count = 0;
 
-int _printf(const char *format, ...) {
-    int count = 0; 
+	va_list args;
 
-    va_list args;
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format) {
-        if (*format == '%') {
-            format+=1;  
-            switch (*format) {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    count+=1;
-                    break;
-                case 's':
-                    count += printf("%s", va_arg(args, char *));
-                    break;
-                case '%':
-                    putchar('%');
-                    count+=1;
-                    break;
-                default:
-                    
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            count+=1;
-        }
-        format++;
-    }
+	if (!format || (format[0] == '%' && format[1] == '\0')) 
+		return (-1);
 
-    va_end(args);
-    return count;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			_putchar(va_arg(args, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			s_count = _puts(va_arg(args, char *));
+			i++;
+			count += (s_count - 1);
+		}
+		else if (format[i+1] == '%')
+		{
+			_putchar('%');
+			i++;
+		}
+		count += 1;
+		}
+	va_end(args);
+	return (count);
 }
