31e7f4a8fba87b23aaee215b29f3100bde6c4ee4
Abouzaid1 <am5715073@gmail.com>
2023-11-13 16:04:54 +0200

    some changes

--- _printf.c
+++ _printf.c
@@ -3,11 +3,11 @@
 #include "main.h"
 
 int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
     int count = 0; 
 
+    va_list args;
+    va_start(args, format);
+	
     while (*format) {
         if (*format == '%') {
             format+=1;  
